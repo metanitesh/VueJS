bbf255cdf2c32b205c959e7269821a8b6f98e023
Nitesh Sharma <nitesh.grep@gmail.com>
2021-10-14 10:34:44 +0530

    Form

---
+++ new-project/src/Form.vue
@@ -0,0 +1,33 @@
+<template>
+ <div id="form">
+   <form @submit.prevent="submitForm">
+     <label for="name"></label><br>
+     <input id="name" type="text" required v-model="name">
+     <button type="submit">Submit</button>
+   </form>
+   <div>{{response}}
+  </div>
+ </div>
+
+</template>
+
+<script>
+export default {
+
+  name: 'Form',
+  data(){
+    return {
+      name: '',
+      response: ''
+    }
+  },
+  methods: {
+    submitForm(e){
+      this.response = `Response from server, ${this.name}`
+    }
+  }}
+</script>
+
+
+
+
--- new-project/src/main.js
+++ new-project/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Form from './Form.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#form',
+  render: h => h(Form)
 })
